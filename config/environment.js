39fc8bed9bed383ff3d4362554c6ed002bf3f5f1
Gaurav <gauraotarpe@gmail.com>
2019-09-21 15:06:39 -0500

    Get price details on ext api call

--- config/environment.js
+++ config/environment.js
@@ -1,7 +1,7 @@
 const environment = {
   mongodbUri: 'mongodb://localhost/productsApi',
   port: process.env.port || 3000,
-  externalApi: (productId) => `http://redsky.target.com/v2/pdp/tcin/${productId}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`,
+  externalApi: (productId) => `http://redsky.target.com/v2/pdp/tcin/${productId}?excludes=taxonomy,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`,
 };
 
 module.exports = environment;
